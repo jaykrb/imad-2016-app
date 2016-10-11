d50c8ad42324712f4b56c8d6d4eabb5246300ab2
jaykrb <jayeshkrishnan3095@gmail.com>
2016-10-11 19:46:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,10 @@
-console.log('Loaded!');
-//move the image
-var img = document.getElementById('madi.png');
-var marginleft =0;
-function moveright () {
-    marginleft = marginleft + 10;
-    img.style.marginleft = marginleft ='px';
-}
-img.onclick - function(){
-    var interval = setInterval(moveright,100);
-    
+var button= document.getElementById('counter');
+var counter =0;
+button.onclick = function (){
+
+
+counter =counter +1;
+var span =document.getElementById('count');
+span.innerHTML =counter.toString();
 };
+
