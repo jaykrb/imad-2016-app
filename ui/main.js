f3503c25a3330e477b165ff18da74d6f3212c24c
jaykrb <jayeshkrishnan3095@gmail.com>
2016-10-11 20:54:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
-var button= document.getElementById('count');
-var count =0;
+var button= document.getElementById('counter');
+var counter =0;
 button.onclick = function (){
 
 
-count =count +1;
+counter =counter+1;
 var span =document.getElementById('count');
-span.innerHTML = count.toString();
+span.innerHTML = counter.toString();
 };
 
