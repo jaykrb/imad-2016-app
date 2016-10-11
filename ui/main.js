598cc5f9e754a323cec0b2ea5ef25cae39c80d23
jaykrb <jayeshkrishnan3095@gmail.com>
2016-10-11 15:12:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //changing text
 var element =document.getElementByid('main.text');
 element.Inourhtml = 'New value';
+//move the image
+var img = document.getElementByid('img');
+img.onclick = function(){
+    img.style.marginleft = '100px';
+    
+};
