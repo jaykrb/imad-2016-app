1d81fb41a0aec77f0278155465dc1c325eb31c0b
jaykrb <jayeshkrishnan3095@gmail.com>
2016-10-11 21:49:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,19 @@
 var button= document.getElementById('counter');
-var counter =0;
 button.onclick = function (){
-
-
-    counter =counter+1;
-    var span =document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLhttpRequest();
+    request.onreadystatechange = function (){
+        if(request.readyState===XMLhttpRequest.DONE){
+            if (request.status===200){
+                var counter = request.responceText;
+                var span =document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET',"http://jaykrb.imad.hasura-app.io/counter",true);
+    request.send(null);
+    
+    
 };
 
